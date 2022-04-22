e5be21f2a3ed5746861bf1fef1acc84980a894b0
manojvuppu <manoj.vuppu@gmail.com>
2022-04-21 22:09:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-v6w9n2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-v6w9n2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  constructor() {}
+}
+
+//     Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+function twoSum(nums: number[], target: number): number[] {
+  let arr = [];
+  nums.forEach((element, index) => {
+    element + nums[index + 1] === target ? (arr = [index, index + 1]) : [];
+  });
+  return arr;
+}

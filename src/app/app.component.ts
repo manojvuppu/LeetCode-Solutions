bf23f54f29148be4babc83c234804a30d1972bb6
manojvuppu <manoj.vuppu@gmail.com>
2022-06-27 17:16:47 -0400

    Palindrome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isPalindrome } from './palindrome';
 import { twoSum } from './twoSum';
 
 @Component({
@@ -8,28 +9,7 @@ import { twoSum } from './twoSum';
 })
 export class AppComponent {
   constructor() {
-    // console.log(isPalindrome(121));
+    console.log(isPalindrome(1001));
     console.log(twoSum([1, -2, 3, 4, 5], 5));
   }
-}
-
-
-// isPalindrome
-
-function isPalindrome(x: number): boolean {
-  if (x > -1) {
-    let input = x.toString().split('');
-    let reverseArray = [];
-    for (let x = 1; x <= input.length; x++) {
-      reverseArray.push(input[input.length - x]);
-    }
-    let count = 0;
-    for (let x = 0; x <= input.length - 1; x++) {
-      if (input[x] === reverseArray[x]) {
-        count = count + 1;
-      }
-    }
-    return count == reverseArray.length ? true : false;
-  }
-  return false;
 }
---
+++ src/app/palindrome.ts
@@ -0,0 +1,17 @@
+export const isPalindrome = (input: number) => {
+  if (input > -1) {
+    const split = input.toString().split('');
+    let reverseArr = [];
+    for (let i = 1; i <= split.length; i++) {
+      reverseArr.push(split[split.length - i]);
+    }
+    let count = 0;
+    for (let i = 0; i <= reverseArr.length - 1; i++) {
+      if (reverseArr[i] === split[i]) {
+        count = count + 1;
+      }
+    }
+    return count == reverseArr.length ? true : false;
+  }
+  return false;
+};

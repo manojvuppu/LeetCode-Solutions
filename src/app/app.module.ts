d429ed3ba3afb1823fec2b7980a0b886a10481df
manojvuppu <manoj.vuppu@gmail.com>
2022-06-30 16:31:48 -0400

    Destructuring, Rest  Patterns & Parameters, Short Circuiting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { JavascriptCodingComponent } from './javascript-coding/javascript-coding.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent,JavascriptCodingComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

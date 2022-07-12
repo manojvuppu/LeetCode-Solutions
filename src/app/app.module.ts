b91dafc8ca4939d503e2216d93aa5dbbbaed44b4
manojvuppu <manoj.vuppu@gmail.com>
2022-07-12 14:18:41 -0400

    code challenge #2 - modern operators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { JavascriptCodingComponent } from './javascript-coding/javascript-coding.component';
+import { ModernOperatorsComponent } from './modern-operators/modern-operators.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent,JavascriptCodingComponent],
+  declarations: [AppComponent,JavascriptCodingComponent,ModernOperatorsComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

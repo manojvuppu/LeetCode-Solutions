d968a424c437070a4748a8a7c6f6e6d5b89e6908
manojvuppu <manoj.vuppu@gmail.com>
2022-07-18 16:46:18 -0400

    Maps Sets Coding Challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { JavascriptCodingComponent } from './javascript-coding/javascript-coding.component';
 import { ModernOperatorsComponent } from './modern-operators/modern-operators.component';
+import { MapsSetsComponent } from './modern-operators/maps-sets-coding/maps-sets/maps-sets.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent,JavascriptCodingComponent,ModernOperatorsComponent],
+  declarations: [AppComponent,JavascriptCodingComponent,ModernOperatorsComponent,MapsSetsComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

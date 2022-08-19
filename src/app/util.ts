046441cda2e5cdf9c0ce71a439326f34bdfe436c
manojvuppu <manoj.vuppu@gmail.com>
2022-08-19 14:07:16 -0400

    Sorted Union Algo

--- src/app/util.ts
+++ src/app/util.ts
@@ -25,24 +25,23 @@ function isUpper(str) {
   return !/[a-z]/.test(str) && /[A-Z]/.test(str);
 }
 
-export function translatePigLatin(str:string){
-  const vowels = ['a','e','i','o','u'];
-  const addedVal = vowels.indexOf(str.charAt(0)) < 0 
-  let stopLoop = false
+export function translatePigLatin(str: string) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const addedVal = vowels.indexOf(str.charAt(0)) < 0;
+  let stopLoop = false;
 
-    str.split('').map(
-    (char)=>{
-      if(vowels.indexOf(char) < 0 && !stopLoop ){
-        console.log(char);
-        str = str.slice(1,str.length) + str.slice(str.indexOf(char),str.indexOf(char)+1) ;
-      }else{
-        stopLoop = true
-      }
+  str.split('').map((char) => {
+    if (vowels.indexOf(char) < 0 && !stopLoop) {
+      console.log(char);
+      str =
+        str.slice(1, str.length) +
+        str.slice(str.indexOf(char), str.indexOf(char) + 1);
+    } else {
+      stopLoop = true;
     }
-  )
-  return addedVal ? str= str +"ay":str =str+"way"
+  });
+  return addedVal ? (str = str + 'ay') : (str = str + 'way');
 }
-
 
 // Search and Replace
 
@@ -53,54 +52,53 @@ function myReplace(sentence, string1, string2) {
     .join(' ');
 }
 
-
 // DNA Pairing
 
-function pairElement(str:string) {
-  return str.split("").map(
-    letter => {
-      if(letter === 'A'){
-        return ["A","T"]
-      }else if (letter === 'T'){
-       return ["T","A"]
-      }else if (letter === 'C'){
-       return ["C","G"]
-      }else if (letter === 'G'){
-       return ["G","C"]
-      }
-     }
-    )
-  }
+function pairElement(str: string) {
+  return str.split('').map((letter) => {
+    if (letter === 'A') {
+      return ['A', 'T'];
+    } else if (letter === 'T') {
+      return ['T', 'A'];
+    } else if (letter === 'C') {
+      return ['C', 'G'];
+    } else if (letter === 'G') {
+      return ['G', 'C'];
+    }
+  });
+}
 
+// ************ Missing letters starts ************
 
+function fearNotLetter(str) {
+  const format = 'abcdefghijklmnopqrstuvwxyz';
+  let min;
+  let max;
+  let exp;
 
-  // ************ Missing letters starts ************
+  min = format.indexOf(str.split('')[0]);
+  max = format.indexOf(str.split('')[str.length - 1]);
+  exp = format.slice(min, max + 1);
 
-  function fearNotLetter(str) {
-    const format = "abcdefghijklmnopqrstuvwxyz";
-    let min;
-    let max;
-    let exp
-  
-    min = format.indexOf(str.split("")[0]);
-    max = format.indexOf(str.split("")[str.length -1]);
-    exp = format.slice(min,max+1)
-  
-  
-    const [man] = exp.split("").filter(
-      strl => str.indexOf(strl) < 0
-    )
-    return man;
-  }
-
-  // tweaked solution one liner
-
-function fearNotLetterOneLiner(str) {
-  
-  const [man] = 'abcdefghijklmnopqrstuvwxyz'.slice('abcdefghijklmnopqrstuvwxyz'.indexOf(str.split("")[0]),'abcdefghijklmnopqrstuvwxyz'.indexOf(str.split("")[str.length -1])+1).split("").filter(
-    strl => str.indexOf(strl) < 0
-  )
+  const [man] = exp.split('').filter((strl) => str.indexOf(strl) < 0);
   return man;
 }
 
-   // ************ Missing Letters Ends ************
+// tweaked solution one liner
+
+function fearNotLetterOneLiner(str) {
+  const [man] = 'abcdefghijklmnopqrstuvwxyz'
+    .slice(
+      'abcdefghijklmnopqrstuvwxyz'.indexOf(str.split('')[0]),
+      'abcdefghijklmnopqrstuvwxyz'.indexOf(str.split('')[str.length - 1]) + 1
+    )
+    .split('')
+    .filter((strl) => str.indexOf(strl) < 0);
+  return man;
+}
+
+// ************ Missing Letters Ends ************
+
+function uniteUnique(...arr) {
+  return [...new Set(arr.reduce((acc, val) => acc.concat(val), []))];
+}

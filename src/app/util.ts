66617121ca3b1c05c8793330fa3f09a6b92cb658
manojvuppu <manoj.vuppu@gmail.com>
2022-08-19 09:21:57 -0400

    piglatin algo

--- src/app/util.ts
+++ src/app/util.ts
@@ -24,3 +24,21 @@ export function spinalCase(str: string) {
 function isUpper(str) {
   return !/[a-z]/.test(str) && /[A-Z]/.test(str);
 }
+
+export function translatePigLatin(str:string){
+  const vowels = ['a','e','i','o','u'];
+  const addedVal = vowels.indexOf(str.charAt(0)) < 0 
+  let stopLoop = false
+
+    str.split('').map(
+    (char)=>{
+      if(vowels.indexOf(char) < 0 && !stopLoop ){
+        console.log(char);
+        str = str.slice(1,str.length) + str.slice(str.indexOf(char),str.indexOf(char)+1) ;
+      }else{
+        stopLoop = true
+      }
+    }
+  )
+  return addedVal ? str= str +"ay":str =str+"way"
+}
